ef1ca91e8622c0984d9701c4b7aad180cc7aa108
console-logs <connect@consolelogs.in>
2023-11-24 14:41:12 +0530

    added shadcn-ui components lib

--- apps/store/src/app/page.tsx
+++ apps/store/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@shared/components/ui/button";
+
 export default async function Home() {
-  return <h1>Hello, World</h1>;
+  return (
+    <div>
+      <h1>Hello Store</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
