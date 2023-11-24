9276142c00bdebae7e24aae9d7a9a3dfe72b21ef
console-logs <connect@consolelogs.in>
2023-11-24 22:31:15 +0530

    added empty cartSizeAction.

---
+++ apps/store/src/actions/cart.ts
@@ -0,0 +1,6 @@
+"use server";
+
+export async function fetchCartSizeAction(): Promise<number> {
+  const cartSize = 0;
+  return cartSize;
+}
--- apps/store/src/components/header/cart.tsx
+++ apps/store/src/components/header/cart.tsx
@@ -1,12 +1,11 @@
+import { fetchCartSizeAction } from "@/actions/cart";
+import { SHOPPING_CART_PAGE } from "@/lib/routes";
 import { Icons } from "@shared/components/Icons";
 import { Button } from "@shared/components/ui/button";
-// import { fetchCartSizeAction } from "@store/src/actions/cartActions";
-import { SHOPPING_CART_PAGE } from "@/lib/routes";
 import Link from "next/link";
 
 export default async function Cart() {
-  const cartItems = 0;
-  // let cartItems = await fetchCartSizeAction();
+  const cartItems = await fetchCartSizeAction();
   return (
     <Button variant={"ghost"} size={"icon"} asChild>
       <Link href={SHOPPING_CART_PAGE}>
