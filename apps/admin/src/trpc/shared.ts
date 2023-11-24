ba57adab188290a2d03e6ea398389854862a5531
console-logs <connect@consolelogs.in>
2023-11-24 14:18:59 +0530

    added admin app.

---
+++ apps/admin/src/trpc/shared.ts
@@ -0,0 +1,17 @@
+import { type AppRouter } from "@/server/api/root";
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+import superjson from "superjson";
+export const transformer = superjson;
+
+function getBaseUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export function getUrl() {
+  return getBaseUrl() + "/api/trpc";
+}
+
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
