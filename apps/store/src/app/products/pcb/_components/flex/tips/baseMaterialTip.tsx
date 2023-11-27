a0d03b6f3fcdb3fdcf31b2570d3e9c4148967a6a
console-logs <connect@consolelogs.in>
2023-11-27 13:23:36 +0530

    base material for flex

---
+++ apps/store/src/app/products/pcb/_components/flex/tips/baseMaterialTip.tsx
@@ -0,0 +1,10 @@
+import HelpPopover from "@/app/products/pcb/_components/common/helpPopover";
+
+export default function BaseMaterialTip() {
+	return (
+		<HelpPopover>
+			Flex PCBs utilize a thin, flexible polymer film as the substrate for the circuitry. Flex PCBs reduce space
+			consumption, and have lower weight.
+		</HelpPopover>
+	);
+}
