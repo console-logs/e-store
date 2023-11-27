0fe8ff247dad1d88d61e00dde5619c17dfd0e2ae
console-logs <connect@consolelogs.in>
2023-11-27 19:30:54 +0530

    improved file handling function.

--- apps/store/src/app/products/pcb/_components/flex/fields/uploadDesignFile.tsx
+++ apps/store/src/app/products/pcb/_components/flex/fields/uploadDesignFile.tsx
@@ -3,17 +3,16 @@ import UploadDesignTip from "@/app/products/pcb/_components/flex/tips/uploadDesi
 import { setDesignFile } from "@/redux/reducers/flexPcbSlice";
 import { Input } from "@shared/components/ui/input";
 import { Label } from "@shared/components/ui/label";
-import { convertToBase64 } from "@shared/lib/utils";
+import { convertFileToBase64 } from "@shared/lib/utils";
 import { useDispatch } from "react-redux";
 
 export default function UploadDesignFile() {
 	const dispatch = useDispatch();
-	// tell the user to fuck off if they are not authenticated. Tell that in bright red text.
 	return (
 		<div>
 			<Label>
 				Upload Design Files(.zip) <UploadDesignTip />
-			</Label>
+			</Label> 
 			<Input
 				required
 				accept=".zip"
@@ -23,8 +22,8 @@ export default function UploadDesignFile() {
 				className="w-full"
 				onChange={async e => {
 					if (e.target.files) {
-						const designFile = e.target.files[0];
-						const base64File = (await convertToBase64(designFile).catch(console.error)) as string;
+						const designFile = e.target.files[0]!;
+						const base64File = (await convertFileToBase64(designFile).catch(console.error)) as string;
 						dispatch(setDesignFile(base64File));
 					}
 				}}
