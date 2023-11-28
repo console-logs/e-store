38b7e6b1b346a11b4cceca19e1381d4796e9f06d
console-logs <connect@consolelogs.in>
2023-11-28 19:20:32 +0530

    updated class statements.

--- apps/store/src/app/products/pcb/_components/assembly/fields/conformalCoating.tsx
+++ apps/store/src/app/products/pcb/_components/assembly/fields/conformalCoating.tsx
@@ -12,6 +12,7 @@ import {
 import { Listbox, Transition } from "@headlessui/react";
 import { Icons } from "@packages/shared/components/Icons";
 import { Label } from "@shared/components/ui/label";
+import clsx from "clsx";
 import { Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -51,9 +52,10 @@ export default function ConformalCoating() {
 								<Listbox.Option
 									key={optionIdx}
 									className={({ active }) =>
-										`relative cursor-default select-none py-1.5 pl-2 pr-4 ${
+										clsx(
+											"relative cursor-default select-none py-1.5 pl-2 pr-4",
 											active && "bg-gray-100"
-										}`
+										)
 									}
 									value={option}>
 									{({ selected }) => (
