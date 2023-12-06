7b137d20976412241046f04dd7b8a660ce050fbf
console-logs <connect@consolelogs.in>
2023-12-06 13:52:56 +0530

    Refactor reset password form validation schema

--- apps/store/src/app/auth/reset-password/_components/resetForm.tsx
+++ apps/store/src/app/auth/reset-password/_components/resetForm.tsx
@@ -1,16 +1,15 @@
 "use client";
 import { RESET_PASSWORD_STEP_2_PAGE } from "@/lib/routes";
+import { resetSchema } from "@/schema/yup";
 import { isClerkAPIResponseError, useSignIn } from "@clerk/nextjs";
 import { Icons } from "@shared/components/Icons";
 import { Button } from "@shared/components/ui/button";
 import { Input } from "@shared/components/ui/input";
 import { Label } from "@shared/components/ui/label";
 import { useToast } from "@shared/components/ui/use-toast";
-import { INVALID_EMAIL } from "@shared/lib/errorMessages";
 import { Field, Form, Formik } from "formik";
 import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-import * as Yup from "yup";
+import { useCallback, useTransition } from "react";
 
 export default function ResetPasswordForm() {
 	const router = useRouter();
@@ -23,54 +22,50 @@ export default function ResetPasswordForm() {
 		email: "",
 	};
 
-	const validationSchema = Yup.object().shape({
-		email: Yup.string()
-			.email(INVALID_EMAIL)
-			.required(INVALID_EMAIL)
-			.matches(/@[^.]*\./, INVALID_EMAIL)
-			.matches(/^\S+$/, INVALID_EMAIL),
-	});
+	const handleOnSubmit = useCallback(
+		(values: { email: string }) => {
+			startTransition(async () => {
+				if (!isLoaded) {
+					return;
+				}
+				try {
+					const firstFactor = await signIn.create({
+						strategy: "reset_password_email_code",
+						identifier: values.email,
+					});
 
-	function handleOnSubmit(values: { email: string }) {
-		startTransition(async () => {
-			if (!isLoaded) {
-				return;
-			}
-			try {
-				const firstFactor = await signIn.create({
-					strategy: "reset_password_email_code",
-					identifier: values.email,
-				});
-
-				if (firstFactor.status === "needs_first_factor") {
-					router.push(RESET_PASSWORD_STEP_2_PAGE);
+					if (firstFactor.status === "needs_first_factor") {
+						router.push(RESET_PASSWORD_STEP_2_PAGE);
+						toast({
+							variant: "default",
+							title: "Check your email",
+							description: "We sent you a 6-digit verification code.",
+							duration: 4000,
+						});
+					}
+				} catch (err: unknown) {
+					let errorMessage: string | undefined = "Something went wrong, please try again later.";
+					if (err instanceof Error) {
+						errorMessage = err.message;
+					} else if (isClerkAPIResponseError(err)) {
+						errorMessage = err.errors[0]?.longMessage;
+					}
 					toast({
-						variant: "default",
-						title: "Check your email",
-						description: "We sent you a 6-digit verification code.",
+						variant: "destructive",
+						title: "Authentication error",
+						description: errorMessage,
 						duration: 4000,
 					});
 				}
-			} catch (err: unknown) {
-				let errorMessage: string | undefined = "Something went wrong, please try again later.";
-				if (err instanceof Error) {
-					errorMessage = err.message;
-				} else if (isClerkAPIResponseError(err)) {
-					errorMessage = err.errors[0]?.longMessage;
-				}
-				toast({
-					variant: "destructive",
-					title: "Authentication error",
-					description: errorMessage,
-					duration: 4000,
-				});
-			}
-		});
-	}
+			});
+		},
+		[startTransition, router]
+	);
+
 	return (
 		<Formik
 			initialValues={initialValues}
-			validationSchema={validationSchema}
+			validationSchema={resetSchema}
 			onSubmit={handleOnSubmit}>
 			{({}) => (
 				<Form className="space-y-3">
--- apps/store/src/schema/yup.ts
+++ apps/store/src/schema/yup.ts
@@ -14,3 +14,11 @@ export const loginSchema = Yup.object().shape({
 		.matches(/^\S+$/, INVALID_EMAIL),
 	password: Yup.string().required(PASSWORD_ERROR),
 });
+
+export const resetSchema = Yup.object().shape({
+	email: Yup.string()
+		.email(INVALID_EMAIL)
+		.required(INVALID_EMAIL)
+		.matches(/@[^.]*\./, INVALID_EMAIL)
+		.matches(/^\S+$/, INVALID_EMAIL),
+});
