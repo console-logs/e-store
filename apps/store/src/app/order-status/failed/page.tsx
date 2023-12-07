5cbb10f8e3c46f7fe409365688a21bfa6e53488d
console-logs <connect@consolelogs.in>
2023-12-07 14:49:12 +0530

    order sucess and error pages.

---
+++ apps/store/src/app/order-status/failed/page.tsx
@@ -0,0 +1,32 @@
+import { REVIEW_ORDER_PAGE } from "@/lib/routes";
+import { Button } from "@shared/components/ui/button";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: "Failed",
+};
+export default function OrderFailed() {
+	return (
+		<div>
+			<div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+				<div className="mx-auto max-w-2xl text-center">
+					<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+						Looks like the order didn&apos;t go through ☹️
+					</h2>
+					<p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
+						But don&apos;t worry, your shopping cart is intack. Click the button below to return to your
+						shopping and try again. We apologise for the inconvenience 😅
+					</p>
+					<div className="mt-10 flex items-center justify-center gap-x-6">
+						<Button
+							variant={"link"}
+							asChild>
+							<Link href={REVIEW_ORDER_PAGE}>Retry →</Link>
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ apps/store/src/app/order-status/success/page.tsx
@@ -0,0 +1,32 @@
+import { HOME_PAGE } from "@/lib/routes";
+import { Button } from "@shared/components/ui/button";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: "Success",
+};
+export default function OrderSuccess() {
+	return (
+		<div>
+			<div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+				<div className="mx-auto max-w-2xl text-center">
+					<h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+						Your order was placed successfully! 🎉
+					</h2>
+					<p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-muted-foreground">
+						Thanks for placing an order with us. Our backend team will check your order and will notify you
+						via email. You can also check your order status in your order history as well.
+					</p>
+					<div className="mt-10 flex items-center justify-center gap-x-6">
+						<Button
+							variant={"link"}
+							asChild>
+							<Link href={HOME_PAGE}>Return to home →</Link>
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
