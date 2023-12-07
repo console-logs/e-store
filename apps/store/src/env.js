5078d4393db7225d5ddb13353a5d2940536d3157
console-logs <connect@consolelogs.in>
2023-12-07 18:57:35 +0530

    uploadthing.

--- apps/store/src/env.js
+++ apps/store/src/env.js
@@ -7,6 +7,8 @@ export const env = createEnv({
 		CLERK_SECRET_KEY: z.string().min(1),
 		MONGODB_URL: z.string().url(),
 		RAZORPAY_KEY_SECRET: z.string().min(1),
+		UPLOADTHING_SECRET: z.string().min(1),
+		UPLOADTHING_APP_ID: z.string().min(1),
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
@@ -23,6 +25,8 @@ export const env = createEnv({
 		MONGODB_URL: process.env.MONGODB_URL,
 		RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
 		NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+		UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+		UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
 	},
 	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 	emptyStringAsUndefined: true,
