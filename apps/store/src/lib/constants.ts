9666bedf9201499bca62bc452c860902597c8171
console-logs <connect@consolelogs.in>
2023-12-08 08:18:27 +0530

    Update constants.ts file

--- apps/store/src/lib/constants.ts
+++ apps/store/src/lib/constants.ts
@@ -1,17 +1,15 @@
 export const WEBSITE_NAME = "E-Store";
-export const SUBTITLE =
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit impedit perferendi.";
+export const SUBTITLE = "Lorem ipsum, dolor sit amet consectetur adipisicing elit impedit perferendi.";
 export const COUNTRY = "India";
 export const BOM_TEMPLATE_STRING =
-  "data:text/csv;base64,TWFudWZhY3R1cmVyUGFydE51bWJlcixEZXNjcmlwdGlvbixRdWFudGl0eQ0KLCwNCg==";
+	"data:text/csv;base64,TWFudWZhY3R1cmVyUGFydE51bWJlcixEZXNjcmlwdGlvbixRdWFudGl0eQ0KLCwNCg==";
 export const FALLBACK_PART_IMAGE =
-  "https://firebasestorage.googleapis.com/v0/b/aries-03.appspot.com/o/images%2Fcomp-img-placeholder.png?alt=media&token=2c3f54f8-c652-43fa-8cab-bd29d878acf1";
+	"https://firebasestorage.googleapis.com/v0/b/aries-03.appspot.com/o/images%2Fcomp-img-placeholder.png?alt=media&token=2c3f54f8-c652-43fa-8cab-bd29d878acf1";
 
 export const ERROR_MSG = {
-  COMPONENT_SEARCH_QUERY_REQUIRED:
-    "Please enter part number, keywords or tech specs to search",
+	COMPONENT_SEARCH_QUERY_REQUIRED: "Please enter part number, keywords or tech specs to search",
 };
 
 //TODO: REPLACE WITH HOSTED PUBLIC URL
-export const PARTS_API = "http://localhost:3993/api/parts?mpn=";
+export const PARTS_API = "https://parts-api-nine.vercel.app/api/parts?mpn=";
 export const OVERHEAD_SHIPPING_CHARGES = 0.0;
